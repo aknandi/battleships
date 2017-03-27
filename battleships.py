ee7aea9d08a4abad57f052349524f59d14758872
Jacob Lever <jacobianism@gmail.com>
2017-03-28 00:59:33 +0100

    Move Ship class into new file and ignore .pyc files (no logic changes)

--- battleships.py
+++ battleships.py
@@ -6,35 +6,7 @@ Possible Extensions:
  Rematches and statistics
 """
 from random import randint
-
-class Ship:
-    def __init__(self, position):
-        self.position = position
-    
-    def getPosition(self):
-        return self.position
-        
-    def getRow(self):
-        return self.position[0]
-        
-    def getColumn(self):
-        return self.position[1]
-        
-    def isOccupyingPosition(self, position):
-        return self.getRow() == position[0] and self.getColumn() == position[1]
-        
-    def isTouching(self, otherShip):
-        if ((abs(self.getRow()-otherShip.getRow()) <= 1)
-            and (abs(self.getColumn()-otherShip.getColumn()) <= 1)):
-            return True
-        else:
-            return False
-            
-    def __eq__(self, other):
-        return self.getPosition() == other.getPosition()
-        
-    def __repr__(self):
-        return "(" + str(self.getRow()) + ", " + str(self.getColumn()) + ")"
+from Ship import Ship
 
 def initialise_board(dim):
     board = []
