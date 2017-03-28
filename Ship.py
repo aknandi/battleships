9ef4c0b176c3100decbe22f9d7b29b1a5640b8fe
Anita Nandi <anita.k.nandi@gmail.com>
2017-03-28 22:46:34 +0100

    Allow battleships of any size

--- Ship.py
+++ Ship.py
@@ -1,25 +1,59 @@
 class Ship:
-    def __init__(self, position):
-        self.position = position
+    def __init__(self, start_position, end_position):
+        self.start_position = start_position
+        self.end_position = end_position
+        self.hits = 0
         
-    def __get_row(self):
-        return self.position[0]
+    def __get_startrow(self):
+        return self.start_position[0]
         
-    def __get_column(self):
-        return self.position[1]
+    def __get_startcolumn(self):
+        return self.start_position[1]
+
+    def __get_endrow(self):
+        return self.end_position[0]
+        
+    def __get_endcolumn(self):
+        return self.end_position[1]
+
+    def get_length(self):
+        if self.__get_startrow() == self.__get_endrow():
+            return abs(self.__get_startcolumn() - self.__get_endcolumn()) + 1
+        else:
+            return abs(self.__get_startrow() - self.__get_endrow()) + 1
+
+    def __get_all_occupied_spaces(self):
+        self.occupied_spaces = []
+        if self.__get_startrow() == self.__get_endrow():
+            row = self.__get_startrow()
+            length = abs(self.__get_startcolumn() - self.__get_endcolumn())
+            for i in range(0,length+1):
+                self.occupied_spaces.append([row,min(self.__get_startcolumn(),self.__get_endcolumn())+i])
+        else:
+            column = self.__get_startcolumn()
+            length = abs(self.__get_startrow() - self.__get_endrow())
+            for i in range(0,length+1):
+                self.occupied_spaces.append([min(self.__get_startrow(),self.__get_endrow())+i,column])
+        return self.occupied_spaces
         
     def is_occupying_position(self, position):
-        return self.__get_row() == position[0] and self.__get_column() == position[1]
+        self.occupied_spaces = self.__get_all_occupied_spaces()
+        for space in self.occupied_spaces:
+            if position[0] == space[0] and position[1] == space[1]:
+                return True
+        return False
         
     def is_touching(self, otherShip):
-        if ((abs(self.__get_row()-otherShip.__get_row()) <= 1)
-            and (abs(self.__get_column()-otherShip.__get_column()) <= 1)):
-            return True
-        else:
-            return False
+        self.occupied_spaces1 = self.__get_all_occupied_spaces()
+        self.occupied_spaces2 = otherShip.__get_all_occupied_spaces()
+        for space1 in self.occupied_spaces1:
+            for space2 in self.occupied_spaces2:
+                if abs(space1[0]-space2[0]) <= 1 and abs(space1[1]-space2[1]) <= 1:
+                    return True
+        return False
             
     def __eq__(self, other):
-        return self.position == other.position
+        return (self.start_position == other.start_position and self.end_position == other.end_position) or (self.start_position == other.end_position and self.end_position == other.start_position)
         
     def __repr__(self):
-        return "(" + str(self.__get_row()) + ", " + str(self.__get_column()) + ")"
+        return "(" + str(self.__get_startrow()) + "," + str(self.__get_startcolumn()) + " to " + str(self.__get_endrow()) + "," + str(self.__get_endcolumn()) + ")"
--- battleships.py
+++ battleships.py
@@ -7,12 +7,28 @@ Possible Extensions:
 from random import randint
 from Board import Board
 from Ship import Ship
+import sys
 
-def generate_ship_position(dim):
+def generate_ship(dimension,length):
     ship_row = randint(0,dimension - 1)
     ship_column = randint(0,dimension - 1)
-    position = [ship_row,ship_column]
-    return Ship(position)
+    start_position = [ship_row,ship_column]
+    end_position = start_position
+    direction = randint(0,3)
+    if direction==0: # right
+        end_position = [ship_row,ship_column+length-1]
+    elif direction==1: # up
+        end_position = [ship_row-(length-1),ship_column]
+    elif direction==2: # left
+        end_position = [ship_row,ship_column-(length-1)]
+    elif direction==3: # down
+        end_position = [ship_row+length-1,ship_column]
+    board = Board(dimension)
+    if board.has_position(end_position):
+        return Ship(start_position,end_position)
+    else:
+        ship = generate_ship(dimension,length)
+        return ship
 
 def can_add_ship(new_ship):
     for current_ship in ship_positions:
@@ -20,27 +36,42 @@ def can_add_ship(new_ship):
             return False
     return True
 
-def ship_hit(guess):
+def is_ship_hit(guess):
     for ship in ship_positions:
         if ship.is_occupying_position(guess):
             return True
     return False
 
-dimension = 4
+def get_ship_hit(guess):
+    for ship in ship_positions:
+        if ship.is_occupying_position(guess):
+            return ship
+
+dimension = 5
 number_of_turns = 4
-number_of_ships = 4
+# An element of [m,n] means m battleships of size n
+initial_ships = [[1,3],[1,2],[3,1]]
 board = Board(dimension)
 print board
 
 ship_positions = []
-for i in range(number_of_ships):
-    new_ship = generate_ship_position(dimension)
-    while (not can_add_ship(new_ship)):
-        new_ship = generate_ship_position(dimension)
-    ship_positions.append(new_ship)
+for number_of_ships,size_of_ship in initial_ships:
+    for i in range(number_of_ships):
+        count = 0
+        new_ship = generate_ship(dimension,size_of_ship)
+        while (not can_add_ship(new_ship)):
+            count += 1
+            if count > 10:
+                print "Cannot fit battleships on board. Try running again or reduce the number of battleships\n"
+                sys.exit()
+            new_ship = generate_ship(dimension,size_of_ship)
+        ship_positions.append(new_ship)
 
-print "There are", number_of_ships, "battleships"
-print ship_positions
+print "There are:"
+for number_of_ships,size_of_ship in initial_ships:
+    print "\t" + str(number_of_ships) + " battleships of size " + str(size_of_ship)
+
+#print ship_positions
 turn = 0
 while turn < number_of_turns:
     print "Turn", turn+1
@@ -52,12 +83,17 @@ while turn < number_of_turns:
         print "Oops, that's not even in the ocean"
     elif (board.get_cell(guess_position) == "X" or board.get_cell(guess_position) == "-"):
         print "You guessed that one already"
-    elif ship_hit(guess_position):
-        print "Congratulations! You sunk a battleship!"
+    elif is_ship_hit(guess_position):
+        print "Congratulations! You hit a battleship!"
         number_of_turns += 1
         board.set_cell(guess_position,"X")
-        ship_positions.remove(Ship([guess_row,guess_column]))
+        ship_hit = get_ship_hit(guess_position)
+        ship_hit.hits += 1
+        if ship_hit.hits == ship_hit.get_length():
+            ship_positions.remove(ship_hit)
+            print "Congratulations! You sunk a whole battleship!"
         if ship_positions == []:
+            print board
             print "You have sunk all the battleships!"
             break
     else:
